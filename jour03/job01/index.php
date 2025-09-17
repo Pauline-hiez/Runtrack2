41e322726a321272048eb97033dfc1b598ebfaa0
Pauline-hiez <pauline.hiez@laplateforme.io>
2025-09-17 15:35:09 +0200

    job01

---
+++ jour03/job01/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$tableau = [200, 204, 173, 98, 171, 404, 459];
+
+foreach ($tableau as $value) {
+    if ($value % 2 == 0) {
+        echo "$value est pair <br>";
+    } else {
+        echo "$value est impair <br>";
+    }
+}
+
+?>
