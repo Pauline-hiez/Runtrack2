0be0b9284671b13d61718c7ebe4fe7b9a66127c8
Pauline-hiez <pauline.hiez@laplateforme.io>
2025-09-17 15:54:09 +0200

    job04

---
+++ jour03/job04/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$str = "Dans l'espace, personne ne vous entend crier";
+
+$nbCaracteres = mb_strlen($str);
+
+echo $str . "<br>" . "La chaîne contient " . $nbCaracteres . " caractères.";
+
+?>
