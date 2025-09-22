4ac387ace6c38894144447e4f62ddd3cd5c88ea0
Pauline-hiez <pauline.hiez@laplateforme.io>
2025-09-22 11:49:08 +0200

    style2

--- jour06/job05/index.php
+++ jour06/job05/index.php
@@ -10,7 +10,7 @@ $style = isset($_POST['style']) ? $_POST['style'] : 'style1';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulaire</title>
-    <link rel="stylesheet" href="<?php echo $style; ?>.css">
+    <link rel="stylesheet" href="<?php echo htmlspecialchars ($style); ?>.css">
 </head>
 <body>
 
