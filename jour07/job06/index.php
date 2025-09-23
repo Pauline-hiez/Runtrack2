3b41aa8a3e4b059d0e79a0e5a806a18873d5e752
Pauline-hiez <pauline.hiez@laplateforme.io>
2025-09-23 13:09:35 +0200

    job06

--- jour07/job06/index.php
+++ jour07/job06/index.php
@@ -1,5 +1,34 @@
 <?php
 
-$function leetSpeak($str) {
-    
+function leetSpeak($str) {
+    $resultat = "";
+
+    $i = 0;
+    while (isset($str[$i])) {
+        $char = $str[$i];
+
+        $maj = $char;
+        if ($char >= "a" && $char <= "z") {
+            $maj = chr(ord($char) - 32);
+        }
+
+        if ($maj === "A") $resultat .= "4";
+        elseif ($maj === "B") $resultat .= "8";
+        elseif ($maj === "E") $resultat .= "3";
+        elseif ($maj === "G") $resultat .= "6";
+        elseif ($maj === "L") $resultat .= "1";
+        elseif ($maj === "S") $resultat .= "5";
+        elseif ($maj === "T") $resultat .= "7";
+
+        else $resultat .= $char;
+
+        $i++;
+    }
+
+return $resultat;
+
 }
+
+echo leetSpeak("LaPlateforme");
+
+?>
