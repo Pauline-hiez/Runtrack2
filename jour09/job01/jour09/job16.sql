c1d85ca091c4b0e86a160c68e941e04faa47c2e2
Pauline-hiez <pauline.hiez@laplateforme.io>
2025-09-26 14:26:00 +0200

    job16-fini

--- jour09/job01/jour09/job16.sql
+++ jour09/job01/jour09/job16.sql
@@ -1,6 +1,6 @@
 USE "jour09";
 
-SELECT e.nom as nom_etage, s.nom as "Biggest Room", s.capacite 
+SELECT e.nom as etage, s.nom as "Biggest Room", s.capacite 
 FROM salles s 
 JOIN etage e ON s.id_etage = e.id 
 WHERE s.capacite =(SELECT MAX(capacite)
