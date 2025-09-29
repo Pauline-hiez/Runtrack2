42b55c79083c98d7f354bb40edaab52d72711e70
Pauline-hiez <pauline.hiez@laplateforme.io>
2025-09-29 13:13:45 +0200

    job04

---
+++ jour10/job04/index.php
@@ -0,0 +1,44 @@
+<?php
+
+try {
+    $pdo = new PDO("mysql:host=localhost;dbname=jour09", "root", "");
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "SELECT prenom, nom, naissance, sexe, email FROM etudiants WHERE prenom LIKE 'T%'";
+    $stmt = $pdo->query($sql);
+
+    echo "<table border='1'>";
+    echo "<tr><th>Prenom</th><th>Nom</th><th>Naissance</th><th>Sexe</th><th>Email</th></tr>";
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo "<tr>";
+        echo "<td>" . $row["prenom"] . "</td>";
+        echo "<td>" . $row["nom"] . "</td>";
+        echo "<td>" . $row["naissance"] . "</td>";
+        echo "<td>" . $row["sexe"] . "</td>";
+        echo "<td>" . $row["email"] . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+}
+
+catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+
+?>
+
+<style>
+table { 
+    font-family: arial; 
+    padding: 10px;
+    display: flex;
+    justify-content: center;
+    border-collapse: collapse;
+}
+tr, td, th {
+    padding: 10px;
+    border-radius: 3px;
+}
+</style>
